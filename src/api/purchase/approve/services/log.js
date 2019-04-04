4646ed23e3651ee08855487cfdfff77fb88208f4
Amós <amos.silva@gmail.com>
2019-04-03 22:33:57 -0300

    Adds configuration of loggi API and log service. Creates an service to confirm Loggi order, and adds a log service to save all transaction to database.

---
+++ src/api/purchase/approve/services/log.js
@@ -0,0 +1,107 @@
+import Sequelize from 'sequelize'
+// {
+//   clientName, address, number, neighborhood, totalPurchase, 
+//   creditCardData: {
+//     numberFromAPI, brand, holder,proofOfSale, tid, authorizationCode, softDescriptor, paymentId, linksData 
+//   }
+// }
+
+const service = class {
+
+  constructor () {
+
+    const maxConnections = 5
+    const minConnections = 1
+    const idleSecondsBeforeClose = 10000
+
+    this.sequelize = new Sequelize(process.env.LOG_DATABASE, process.env.LOG_LOGIN, process.env.LOG_PASSWORD, {
+      host: process.env.LOG_HOST,
+      dialect: process.env.LOG_DATABASE_TYPE,
+    
+      pool: {
+        max: maxConnections,
+        min: minConnections,
+        idle: idleSecondsBeforeClose
+      }
+    })
+
+    this.transactionLog = sequelize.define('transactionLog', {
+      clientName: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      number: {
+        type: Sequelize.STRING
+      },
+      neighborhood: {
+        type: Sequelize.STRING
+      },
+      totalPurchase: {
+        type: Sequelize.DECIMAL
+      },
+      creditCardNumberFromAPI:{
+        type: Sequelize.STRING
+      },
+      creditCardBrand:{
+        type: Sequelize.STRING
+      },
+      creditCardHolder:{
+        type: Sequelize.STRING
+      },
+      creditCardProofOfSale:{
+        type: Sequelize.STRING
+      },
+      creditCardTid:{
+        type: Sequelize.STRING
+      },
+      creditCardAuthorizationCode:{
+        type: Sequelize.STRING
+      },
+      creditCardSoftDescriptor:{
+        type: Sequelize.STRING
+      },
+      creditCardPaymentId:{
+        type: Sequelize.STRING
+      },
+      creditCardLinksData:{
+        type: Sequelize.TEXT
+      }
+    })
+  }
+
+  save(transactionData) {
+    return new Promise((resolve, reject)=> {
+      let transactionLog = this.transactionLog
+
+      transactionLog.sync().then( () => {
+        transactionLog.create({
+          clientName: transactionData.clientName,
+          address: transactionData.address,
+          number: transactionData.number,
+          neighborhood: transactionData.neighborhood,
+          totalPurchase: transactionData.neighborhood,
+          creditCardNumberFromAPI: transactionData.creditCard.numberFromAPI,
+          creditCardBrand: transactionData.creditCard.brand,
+          creditCardHolder: transactionData.creditCard.holder,
+          creditCardProofOfSale: transactionData.creditCard.proofOfSale,
+          creditCardTid: transactionData.creditCard.tid,
+          creditCardAuthorizationCode: transactionData.creditCard.authorizationCode,
+          creditCardSoftDescriptor: transactionData.creditCard.softDescriptor,
+          creditCardPaymentId: transactionData.creditCard.paymentId,
+          creditCardLinksData: transactionData.creditCard.linksData
+        })
+        .then(()=>{
+          resolve()
+        })
+        .catch(()=>{
+          reject()
+        })
+      })
+    })
+    
+  }
+}
+
+export default service
