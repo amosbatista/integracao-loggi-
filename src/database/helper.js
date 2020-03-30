0201c6b554f8d97d389cb0ad503a95092203758d
Amós <amos.silva@gmail.com>
2020-03-30 12:44:33 -0300

    fix(database): reduce connections

--- src/database/helper.js
+++ src/database/helper.js
@@ -2,18 +2,18 @@ import Sequelize from 'sequelize'
 
 const service = (tableName, model) => {
 
-  const maxConnections = 20
+  const maxConnections = 2
   const minConnections = 0
-  const idleSecondsBeforeClose = 1000
+  const idleSecondsBeforeClose = 1
   
   const sequelize = new Sequelize(process.env.LOG_DATABASE, process.env.LOG_LOGIN, process.env.LOG_PASSWORD, {
     host: process.env.LOG_HOST,
     dialect: process.env.LOG_DATABASE_TYPE,
-  
+    logging: false,
     pool: {
       max: maxConnections,
       min: minConnections,
-      idle: idleSecondsBeforeClose
+      idle: idleSecondsBeforeClose,
     }
   })
   
