8fc881ef6e4b30aa292300857ef17c8d97e80823
Amós Batista <amos.silva@gmail.com>
2021-08-29 17:33:35 -0300

    git commiçt --a

---
+++ src/v1/delivery/clickEntregas/clickEntregasCreateOrderService.js
@@ -0,0 +1,79 @@
+import httpReq from 'superagent'
+import notaryAddress from '../../notary/entity'
+
+const DESCRICAO = "Documentos a serem retirados e levados para o 20º cartório.";
+const PESO_TOTAL = "2";
+const MUST_ENABLE_NOTIFICATION = true;
+
+const service = (addressData, servicesData) => {
+
+  return new Promise ( (resolve, reject) => {
+
+    const query = `
+      {
+        "matter": "${DESCRICAO}",
+        "total_weight_kg": ${PESO_TOTAL},
+        "is_contact_person_notification_enabled": ${MUST_ENABLE_NOTIFICATION},
+        "points": [
+            {
+                "note": "${addressData.addressComplement}",
+                "address":"${addressData.completeAddress}"
+                "contact_person":{
+                    "name": "${servicesData.clientName}",
+                    "phone":"${servicesData.clientPhone}"
+                }
+            },
+            {
+                "address": "${notaryAddress.completeAddress}",
+                "contact_person":{
+                    "name": "${notaryAddress.contactName}",
+                    "phone":"${notaryAddress.contactPhone}"
+                }
+            }
+        ]
+    }
+    `
+
+    httpReq.post(`${process.env.CLICK_ENTREGAS_API}/create-order`)
+    .send(query)
+    .set('X-DV-Auth-Token', process.env.CLICK_ENTREGAS_TOKEN)
+    .set('Content-Type', 'application/json')
+    .end((err, apiRes) => {
+      
+      if(err){
+        reject({
+          message: 'Erro ao fazer requisição para criar entrega na API da Click Entregas',
+          object: JSON.stringify(err)
+        })
+
+        return
+      }
+
+      if(apiRes.body.errors){
+        reject({
+          message: 'Erro interno da API Click Entregas ao criar entrega',
+          object: JSON.stringify(apiRes.body.errors)
+        })
+
+        return
+      }
+
+      if(apiRes.body.warnings.length > 0){
+        reject({
+          message: 'Está faltando informações para finalizar a entrega.',
+          object: apiRes.body.parameter_warnings
+        })
+
+        return
+      }
+      
+      resolve({
+        "loggiOrderId": apiRes.body.order.order_id,
+        "packageId": apiRes.body.order.order_id.points[0].point_id,
+      })
+      
+    })
+  })
+}
+
+export default service
--- src/v1/notary/entity.js
+++ src/v1/notary/entity.js
@@ -6,6 +6,12 @@ const notaryData = {
   state: 'São Paulo',
   country: 'Brazil',
   countryShortName: 'BR',
-  postalCode: '04534013'
+  postalCode: '04534011',
+  lat:  -23.5857434,
+  lng: -46.6785174,
+  completeAddress: 'R. Joaquim Floriano, 889 - Itaim Bibi, São Paulo - SP, 04534-011, Brazil',
+  complement: "",
+  contactName: "Alcione",
+  contactPhone: "551199999999",
 }
 export default notaryData
