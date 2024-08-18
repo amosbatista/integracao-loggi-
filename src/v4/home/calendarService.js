882dea97a68ce8f24c14bf3a2f563b1ddc3b366c
Amós Batista <amos.silva@gmail.com>
2024-08-18 16:00:27 -0300

    feat(calendar): lunar

--- src/v4/home/calendarService.js
+++ src/v4/home/calendarService.js
@@ -1,10 +1,12 @@
 import national from "../calendar/calendars/national"
+import lunar from "../calendar/calendars/moon"
 import Service from '../calendar/calendar.service'
 
 export default () => {
   const service = Service();
 
   return {
-    national: service.getTheNextDateInsideCalendar(national)
+    national: service.getTheNextDateInsideCalendar(national),
+    lunar: service.getTheNextDateInsideCalendar(lunar)
   }
 }
