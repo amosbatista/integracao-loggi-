2d9228dcf71fef64b7a6e70c06e748dc884d6874
Amós Batista <amos.silva@gmail.com>
2024-09-01 15:20:02 -0300

    feat: data de publicação

--- src/v4/posts/formatPostForPage.helper.js
+++ src/v4/posts/formatPostForPage.helper.js
@@ -1,12 +1,16 @@
+import moment from "moment";
+
 export default (post) => {
-  const { title,  feature_image, excerpt, slug, html, feature_image_alt  } = post;
-  
+  const { title,  feature_image, excerpt, slug, html, feature_image_alt, published_at  } = post;
+  const dateFormat = 'DD/MM/YYYY'
+
   return { 
     title, 
     thumbnail: feature_image, 
     description: excerpt, 
     url: slug,
     content: html,
-    thumb_alt: feature_image_alt
+    thumb_alt: feature_image_alt,
+    date: moment(published_at).format((dateFormat))
   };      
 }
